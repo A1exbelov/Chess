3697f909467972d48b4058b99f2938a18caffc99
A1exbelov <a1exandr00bel0v@gmail.com>
2023-05-18 01:28:45 +0400

    Add struct position

---
+++ Chess.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+const int N = 8;
+// Options for shifting the knight by cells
+int dx[N] = { 2, 2, 1, 1, -1, -1, -2, -2 };
+int dy[N] = { 1, -1, 2, -2, 2, -2, 1, -1 };
+
+struct Position {
+    // Coordinates and distance to the required position
+    int x, y, dist;
+    // The path to the current position
+    string path;
+    Position(int x, int y, int dist, string path) : x(x), y(y), dist(dist), path(path) {}
+};
+
+
+
+int main() {
+
+}
