49d953a8d8ade5bd874d0e3584ed68bee110b5df
A1exbelov <a1exandr00bel0v@gmail.com>
2023-05-19 01:22:27 +0400

    Add Depth breadth search and main function

--- Chess.cpp
+++ Chess.cpp
@@ -16,13 +16,48 @@ struct Position {
     Position(int x, int y, int dist, string path) : x(x), y(y), dist(dist), path(path) {}
 };
 
+// Function for finding the shortest path between two positions
+void shortestPath(string start, string end) {
+    // Array for tracking visited positions
+    vector<vector<bool>> visited(N, vector<bool>(N, false));
+    queue<Position> q;
+    int startX = start[0] - 'A', startY = start[1] - '1';
+    int endX = end[0] - 'A', endY = end[1] - '1';
+    Position startPos(startX, startY, 0, start);
+    visited[startX][startY] = true;
+    q.push(startPos);
 
-int main() {
-    string start, end;
-    cout << "Enter starting position (e.g. A2): ";
-    cin >> start;
-    cout << "Enter ending position (e.g. E6): ";
-    cin >> end;
-    shortestPath(start, end);
-    return 0;
-}
+    while (!q.empty()) {
+        Position currPos = q.front();
+        q.pop();
+        // get coordinates, distance and path for the current position from currPos
+        int currX = currPos.x, currY = currPos.y, currDist = currPos.dist;
+        string currPath = currPos.path;
+
+        if (currX == endX && currY == endY) {
+            cout << "Shortest path: " << currPath << endl;
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            int newX = currX + dx[i], newY = currY + dy[i];
+            if (newX >= 0 && newX < N && newY >= 0 && newY < N && !visited[newX][newY]) {
+                visited[newX][newY] = true;
+                string newPath = currPath + " " + char(newX + 'A') + char(newY + '1');
+                Position newPos(newX, newY, currDist + 1, newPath);
+                q.push(newPos);
+            }
+        }
+    }
+    cout << "No path found" << endl;
+
+
+    int main() {
+        string start, end;
+        cout << "Enter starting position (e.g. A2): ";
+        cin >> start;
+        cout << "Enter ending position (e.g. E6): ";
+        cin >> end;
+        shortestPath(start, end);
+        return 0;
+    }
